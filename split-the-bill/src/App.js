c394f9c389b27c4b90048e98cd509ba5702472e2
Elijah Conley <elijahandaleciael@gmail.com>
2019-09-17 22:06:56 -0400

    react app setup

---
+++ split-the-bill/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Edit & then save.
+        </p>
+    </div>
+  );
+}
+
+export default App;
