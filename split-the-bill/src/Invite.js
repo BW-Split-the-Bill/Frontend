611762abb49d2a5a4c68d764dbc8fba7d5df5526
Elijah Conley <elijahandaleciael@gmail.com>
2019-09-22 20:13:34 -0400

    work on invite js need to push data to send email

--- split-the-bill/src/Invite.js
+++ split-the-bill/src/Invite.js
@@ -1,1 +1,42 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+
+
+const  Invite = () => (
+    <div>
+      <h1>Invite Your Group</h1>
+      <Formik
+        initialValues={{ email: ''}}
+        validate={values => {
+          let errors = {};
+          if (!values.email) {
+            errors.email = 'Required';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.email(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert("invite has been sent");
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="div" />
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+
+export default Invite;
