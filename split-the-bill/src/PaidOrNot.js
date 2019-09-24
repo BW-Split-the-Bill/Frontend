48112864fb9070699bac19764bc06f893fe775ee
Dallas James <dallasjames42@gmail.com>
2019-09-23 19:46:39 -0600

    done with the PaidOrNot until i get some more data

--- split-the-bill/src/PaidOrNot.js
+++ split-the-bill/src/PaidOrNot.js
@@ -7,11 +7,51 @@ const Paid = () => {
     const Done = () => {
         setOwed(0);
     }
+// idea for some payment options might get rid of later
+    const Paid1 = () => {
+        setOwed(owed - 1)
+    }
 
+    const Paid5 = () => {
+        setOwed(owed - 5)
+    }
+    
+    const Paid10 = () => {
+        setOwed(owed - 10)
+    }
+    
+    const Paid20 = () => {
+        setOwed(owed - 20)
+    }
+    
+    const Paid50 = () => {
+        setOwed(owed - 50)
+    }
+
+    const Paid100 = () => {
+        setOwed(owed - 100)
+    }
+// end
     return(
+        <div>
+        <h3>Bill owes you:</h3>
+        <br/>
+        <h2>${owed}</h2>
+{/*also part of the idea*/}
         <div className='owed'>
-        <h4>Bill owes you ${owed}</h4>
-        <button onClick={Done}>Paid</button>
+            <button onClick={Paid1}>Paid $1</button>
+            <button onClick={Paid5}>Paid $5</button>
+            <button onClick={Paid10}>Paid $10</button>
+        </div>
+        <div className='owed'>
+            <button onClick={Paid20}>Paid $20</button>
+            <button onClick={Paid50}>Paid $50</button>
+            <button onClick={Paid100}>Paid $100</button>
+         </div>
+{/*end*/}
+        <div className='owed'>
+            <button className='owed' onClick={Done}>Paid Off</button>
+        </div>
         </div>
     )
 }
