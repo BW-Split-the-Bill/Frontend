780d985d0715e6d8a45d467584b85c7e31f42be0
Dallas James <dallasjames42@gmail.com>
2019-09-23 18:43:26 -0600

    waiting on data from the calc app

--- split-the-bill/src/PaidOrNot.js
+++ split-the-bill/src/PaidOrNot.js
@@ -3,14 +3,14 @@ import React, { useState } from 'react';
 
 const Paid = () => {
     const [owed, setOwed] = useState();
-    
+
     const Done = () => {
         setOwed(0);
     }
 
     return(
         <div className='owed'>
-        <h4>Bill owes you {owed}</h4>
+        <h4>Bill owes you ${owed}</h4>
         <button onClick={Done}>Paid</button>
         </div>
     )
